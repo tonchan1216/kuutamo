5868c9d9201582bf30d6e3c96231e849a0c90eff
tonchan1216 <viva.la.vida27315@gmail.com>
2016-11-17 01:06:58 +0900

    style.css強制読み込み

--- puro/header.php
+++ puro/header.php
@@ -14,7 +14,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<link href="<?php echo get_template_directory_uri();?>/style.css" rel="stylesheet" type="text/css" />
 <?php wp_head(); ?>
 </head>
 
